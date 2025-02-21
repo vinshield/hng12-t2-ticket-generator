18fb238e9b3c23425255dc94c90508ed3334d59a
Jesse <jeslelacodes@gmail.com>
2025-02-21 22:36:21 +0100

    refactor: improve header component layout and styling for better readability

--- components/shared/Header.jsx
+++ components/shared/Header.jsx
@@ -5,35 +5,35 @@ import { Button } from "../ui/button";
 import { MoveRight } from "lucide-react";
 
 const headerLinks = [
-	{ name: "Events", url: "/" },
-	{ name: "My Tickets", url: "/events" },
-	{ name: "About", url: "/events" },
+  { name: "Events", url: "/" },
+  { name: "My Tickets", url: "/" },
+  { name: "About", url: "/" },
 ];
 
 const Header = () => {
-	return (
-		<div className='flex justify-center'>
-			<div id='header'>
-				<Logo className='w-24 h-auto' />
-				<nav>
-					<ul className='text-white justify-between hidden md:flex gap-5 '>
-						{headerLinks.map((item) => (
-							<li key={item.name}>
-								<Link href={item.url} className='whitespace-nowrap'>
-									{item.name}
-								</Link>
-							</li>
-						))}
-					</ul>
-				</nav>
-				<div>
-					<Button variant='secondary' size='lg'>
-						My Tickets <MoveRight />
-					</Button>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex justify-center">
+      <div id="header">
+        <Logo className="h-auto w-24" />
+        <nav>
+          <ul className="hidden justify-between gap-6 font-serif text-white md:flex">
+            {headerLinks.map((item) => (
+              <li key={item.name}>
+                <Link href={item.url} className="whitespace-nowrap">
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div>
+          <Button variant="secondary" size="lg">
+            My Tickets <MoveRight />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
